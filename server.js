db011d6e6d9ad4ff71678f975efc28a11effd70b
unknown <nazim_huseynov@example.com>
2023-04-13 12:31:18 +0400

    modified server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Product = require('./models/productModel')
 const app = express()
 const port = 3000
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World From Windows Machine!')
@@ -12,9 +15,28 @@ app.get('/blog', (req, res) => {
     res.send('First of all, Welcome to my blog! My name is Nazim :)')
   })
 
+app.get('/products', async(req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products)
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+})
+
+app.post('/products', async(req, res) => {
+  try {
+      const product = await Product.create(req.body)
+      res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({message: error.message})
+  }
+})
+
 //connect our app with database, for this we need to use package called mongos
 mongoose.set("strictQuery", false)
-mongoose.connect('mongodb+srv://root:123456789@nhuseynov-api.foo8gqn.mongodb.net/node-api?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://nhuseynov:TestTestovTestik@nhuseynov-api.foo8gqn.mongodb.net/node-api?retryWrites=true&w=majority')
 .then(() => {
     console.log('connected to mongodb')
     app.listen(port, () => {
